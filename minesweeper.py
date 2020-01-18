f7d08cc8b86a43a050b6fe00b9b8e7091c9d84b9
Michael Belland <belland.michael@gmail.com>
2020-01-17 23:51:41 -0600

    Adding a basic board representation.

--- minesweeper.py
+++ minesweeper.py
@@ -4,10 +4,37 @@ class Board:
         self.numRows = numRows
         self.numColumns = numColumns
         self.numMines = numMines
+        self.gameOver = False
+
+        self.board = self.makeNewBoard(self.numRows, self.numColumns)
 
     def printState(self):
-        print "This board has %(nR)x rows, %(nC)x columns, and %(nM)x mines." % {"nR": self.numRows, "nC": self.numColumns, "nM": self.numMines}
+        print "This board has %(nR)x row(s), %(nC)x column(s), and %(nM)x mine(s)." % {"nR": self.numRows, "nC": self.numColumns, "nM": self.numMines}
+        self.printBoard()
 
+    """
+    Check standard docstring procedure
+
+    Example: rows = 2, cols = 3
+
+    [[X, X, X], 
+     [X, X, X]] 
+
+    Make sure to take care with indices!
+    Confusion from variables sharing names with instance variables?
+    """
+    def makeNewBoard(self, numRows, numColumns):
+        newBoard = [0] * numRows
+        for i in xrange(numRows):
+            newBoard[i] = [0] * numColumns
+        return newBoard
+
+    def printBoard(self):
+        print self.board
+
+
+
+#if using imports, this would be main.py
 #initialize the board
 numRows = input("How many rows in the board? ")
 numColumns = input("How many columns in the board? ")
@@ -15,3 +42,15 @@ numMines = input("How many mines in the board? ")
 
 gameBoard = Board(numRows, numColumns, numMines)
 gameBoard.printState()
+
+
+#game loop here; keep accepting inputs until game finished
+#while not gameBoard.gameOver():
+#    gameBoard.step()
+
+print "Program execution complete."
+
+#testing ideas:
+#invalid inputs for board size or mine count
+#  - what if these inputs aren't numbers?
+#  - what if there are too many mines (numMines > numRows * numColumns)?
